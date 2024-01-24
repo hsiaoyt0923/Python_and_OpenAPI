b324ec0712c0df7cbb00b4a8f791013d201a8d87
Eddie Hsiao <xcvb1321@gmail.com>
2024-01-24 15:23:22 +0800

    modify

--- pico_w/lesson11_1.py
+++ pico_w/lesson11_1.py
@@ -6,7 +6,7 @@ import urequests
 red_led = Pin(15,mode=Pin.OUT)
 btn = Pin(14,mode=Pin.PULL_DOWN)
 is_press = False
-#connect()
+connect()
 
 def getCurrentTime():
     times_tuple = time.localtime()
@@ -37,13 +37,15 @@ while True:
         time.sleep_ms(50)
         if btn.value() == False:
             if is_press == True:     
-                print('release')                
-                print(getCurrentTime())
-                print(getTemperature())
-                print(getLightValue())
+                print('release')
+                
+                current_time = getCurrentTime()
+                celsius = getTemperature()
+                light = getLightValue()
+                
                 is_press = False
-                '''
-                url_str = 'https://自已的網址/pico_w/2024-01-22 16:02:10?address=chicken_KFC&celsius=15.386'
+                
+                url_str = f'https://openapi-pico.onrender.com//pico_w/{current_time}?address=chicken_house&celsius={celsius}&light={light}'
                 try:
                     response = urequests.get(url_str)
                     pass
@@ -56,7 +58,7 @@ while True:
                     else:
                         print("傳送失敗(server出現錯誤)")
                     response.close()
-                '''
+                
             
         
             red_led.value(0)
