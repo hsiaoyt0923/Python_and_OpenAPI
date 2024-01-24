5f2d7ac1c4f3313ec16fa72e771d538fcc438f19
Eddie Hsiao <xcvb1321@gmail.com>
2024-01-24 16:23:20 +0800

    modify

--- openAPI/main1.py
+++ openAPI/main1.py
@@ -3,6 +3,8 @@ from fastapi import FastAPI
 import redis
 import os
 from dotenv import load_dotenv
+from pydantic import BaseModel
+
 load_dotenv()
 redis_conn = redis.Redis.from_url(os.environ.get('REDIS_HOST_PASSWORD'))
 
@@ -13,26 +15,11 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
-@app.get("/items/{item_id}")
-async def get_item(item_id:int):
-    print(f"使用者輸入了:{item_id}")
-    return {"item_id":item_id}
-
 @app.get("/items/{date}/{celsius}")
 async def get_item(date:str,celsius:float):
     print(f"日期:{date}")
     print(f"溫度:{celsius}")
     return {"日期":date,"攝氏溫度":celsius}
-
-
-
-fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
-@app.get("/items/")
-async def read_item(skip: int = 0, limit: int = 10):
-    return fake_items_db[skip : skip + limit]
-
-
-
 
 @app.get("/pico_w/{date}")
 async def read_item(date:str ,address:str,celsius:float,light:float):
@@ -55,3 +42,26 @@ async def read_item(date:str ,address:str,celsius:float,light:float):
     print(light_get)
     
     return {"狀態":"儲存成功"}
+
+
+class Pico_w(BaseModel):
+    date:str
+    address:str
+    temperature:float
+    light:float
+
+
+@app.get("/pico_w/")
+async def read_item(count:int=1):
+    date_list = redis_conn.lrange('pico_w:date',-count,-1)
+    dates = [date.decode() for date in date_list]
+    all_Data:[Pico_w] = []
+    for date in dates:
+        address_get = redis_conn.hget('pico_w:address',date).decode()
+        temperature_get = redis_conn.hget('pico_w:temperature',date).decode()
+        light_get = redis_conn.hget('pico_w:light',date).decode()
+        item = Pico_w(date=date,address=address_get,temperature=float(temperature_get),light=float(light_get))
+        all_Data.append(item)
+
+    
+    return all_Data
